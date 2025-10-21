2e801e017cf3b5df9c841c2fb12e3d7055881446
Agus Novianto <agus.n.jogja@gmail.com>
2025-10-21 12:49:02 +0700

    feat: featch data from BE web applications

--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Post from "../Post"
 import Modal from "../Modal"
 import NewPost from "../NewPost"
@@ -6,9 +6,29 @@ import styles from "./PostList.module.css"
 
 export default function PostList({ isPosting, onStopPosting }) {
     const [posts, setPosts] = useState([])
+    const [isFetching, setIsFetching] = useState(false)
+
+    useEffect(() => {
+        async function fetchPosts(){
+            setIsFetching(true)
+            const res = await fetch('http://localhost:8080/posts')
+            const resData = await res.json()
+
+            setPosts(resData.posts)
+            setIsFetching(false)
+        }
+
+        fetchPosts()
+    }, [])
 
     function addPostHandler(postData) {
-        setPosts((existingPost) => ([postData, ...existingPost]))
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
     }
 
     return (<>
@@ -22,18 +42,23 @@ export default function PostList({ isPosting, onStopPosting }) {
             </Modal>
         }   
         {
-            posts.length > 0 && (
+            !isFetching && posts.length > 0 && (
                 <ul className={styles.ul}>
-                    { posts.map(post => (<Post key={post.body} author={post.author} body={post.body}/>)) }
+                    { posts.map(post => (<Post key={post.id} author={post.author} body={post.body}/>)) }
                 </ul>
             )
         }
         {
-            posts.length === 0 && (
+            !isFetching && posts.length === 0 && (
                 <div>
                     <h1>Opss...</h1>
                     <p>No data here...</p>
                 </div>
+            )
+        }
+        {
+            isFetching && (
+                <p>Loading....</p>
             )
         }
         
